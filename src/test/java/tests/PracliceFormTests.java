cda0e3d09fe878f732d7486ca0a76837786909ab
lybov <lyuba.n3@gmail.com>
2025-04-10 11:28:28 +0300

    Changed Configuration.holdBrowserOpen = true to Configuration.holdBrowserOpen = false

--- src/test/java/tests/PracliceFormTests.java
+++ src/test/java/tests/PracliceFormTests.java
@@ -15,7 +15,7 @@ public class PracliceFormTests {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.holdBrowserOpen = true;
+        Configuration.holdBrowserOpen = false;
         Configuration.timeout = 5000; // default 4000
     }
 
